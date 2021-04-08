cf5edbd0a0857630e892657be5bfb984741f4e6e
Daniel Sazonov <p.294803@gmail.com>
2021-04-08 10:34:22 +0300

    Init commit

---
+++ config.py
@@ -0,0 +1,6 @@
+VERSION = {
+    'major': 0,
+    'minor': 0,
+    'micro': 0
+}
+AUTHOR = 'Dan Sazonov'
---
+++ main.py
@@ -0,0 +1,4 @@
+import config
+
+print('folder_icons readme generator v{0}.{1}.{2} by {3}'.format(config.VERSION['major'], config.VERSION['minor'],
+                                                               config.VERSION['micro'], config.AUTHOR))
